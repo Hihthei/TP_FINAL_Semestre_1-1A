bbee06e9bae0ed4deda07d7a3583e7ad6c89fc67
Hihthei <115265603+Hihthei@users.noreply.github.com>
2023-01-16 18:28:28 +0100

    Add files via upload

--- Math.c
+++ Math.c
@@ -15,31 +15,36 @@ Vec2 Vec2_Set(float x, float y)
 
 Vec2 Vec2_Add(Vec2 v1, Vec2 v2)
 {
-	return Vec2_Set(v1.x+v2.x, v1.y+v2.y);
+    Vec2 Vec2_Zero = { v1.x + v2.x, v1.y + v2.y };
+    return Vec2_Zero;
 }
 
 Vec2 Vec2_Sub(Vec2 v1, Vec2 v2)
 {
-	return Vec2_Set(v1.x-v2.x, v1.y-v2.y);
+    Vec2 Vec2_Zero = { v1.x - v2.x, v1.y - v2.y };
+    return Vec2_Zero;
 }
 
 Vec2 Vec2_Scale(Vec2 v, float s)
 {
-	return Vec2_Set(v.x*s, v.y*s);
+    Vec2 Vec2_Zero = { v.x * s, v.y * s };
+    return Vec2_Zero;
 }
 
 float Vec2_Length(Vec2 v)
 {
-	return sqrt(v.x*v.x + v.y*v.y);
+    return (sqrtf((pow( v.x, 2 ) + pow( v.y, 2 ))));
 }
 
 Vec2 Vec2_Normalize(Vec2 v)
 {
-	float l = Vec2_Length(v);
-	return Vec2_Set(v.x/l, v.y/l);
+    float v_norme = Vec2_Length( v );
+    Vec2 Vec2_Zero = { ( v.x / v_norme ), ( v.y / v_norme )};
+    return Vec2_Zero;
 }
 
 float Vec2_Distance(Vec2 v1, Vec2 v2)
 {
-	return Vec2_Length(Vec2_Sub(v1, v2));
+    Vec2 Distance = Vec2_Sub( v1, v2 );
+    return Vec2_Length( Distance );
 }
