1567314ba7924fdd2f1209b7c8828e9d7800f507
N1coc4colA <50690360+N1coc4colA@users.noreply.github.com>
2023-01-19 11:05:27 +0100

    Add files via upload

--- Settings.h
+++ Settings.h
@@ -21,6 +21,13 @@ static inline void nofunc() {};
 #  include "SDL_image.h"
 //#  include "SDL_ttf.h"
 //#  include "SDL_mixer.h"
+
+//These are not provided in MS libs.
+
+# define M_PI		3.14159265358979323846	/* pi */
+# define M_PI_2		1.57079632679489661923	/* pi/2 */
+# define M_PI_4		0.78539816339744830962	/* pi/4 */
+
 #else
 #  include <SDL2/SDL.h>
 #  include <SDL2/SDL_image.h>
